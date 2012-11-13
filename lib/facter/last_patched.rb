7f8d36724393194532eb7256d9857ee313a962e5
James Scott <scottjab@gmail.com>
2012-11-12 19:59:45 -0500

    Added support for Debian based distros.

--- lib/facter/last_patched.rb
+++ lib/facter/last_patched.rb
@@ -5,9 +5,16 @@ Facter.add("last_patched") do
         family = Facter.value("osfamily")
         case family
         when /Suse|RedHat/
-            timestamp = Facter::Util::Resolution.exec('time rpm -qa --queryformat '%{installtime}\n' | sort -n | tail -1
-')
-            Time.at(timestamp).to_time.iso8601
+            command = "rpm -qa --queryformat '%{installtime}\\n' | sort -n | tail -1"
+            timestamp = Facter::Util::Resolution.exec(command).chomp()
+            Time.at(Integer(timestamp)).iso8601
+        when /Debian/
+            #This command shamelessly taken from some example, looking for a better way to do this.
+            command = "ls --time-style='+%s' -l /var/log/apt | awk '{print $6}' | sort -u | tail -1"
+            timestamp = Facter::Util::Resolution.exec(command).chomp()
+            Time.at(Integer(timestamp)).iso8601
         else
             "%s is not Supported." % Facter.value("osfamily")
+        end
+    end
 end
